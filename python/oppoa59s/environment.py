d3265ffc014f95f473465d0a1731730943409aff
huangzetao <huangzetao@kuaishou.com>
2019-05-10 10:35:34 +0800

    add behave

---
+++ python/oppoa59s/environment.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from appium import webdriver
+import time
+
+def before_all(context):
+    # context.config.setup_logging()
+    pass
+
+def before_feature(context,feature):
+    desired_caps = {}
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '5.1'
+    desired_caps['deviceName'] = 'A59s_11_A12_180302'
+    desired_caps['autoGrantPermissions'] = True
+    desired_caps['appPackage'] = 'com.kwai.sogame'
+    desired_caps['udid'] = 'HQ5HLFW899999999'
+    desired_caps['automationName'] = 'UiAutomator2'
+    desired_caps['appActivity'] = 'com.kwai.sogame.combus.launch.SogameLaunchActivity'
+    desired_caps['systemPort'] = 8201
+    desired_caps['noReset'] = True
+    # desired_caps['app'] = '/Users/huangzetao/Desktop/Sogame-2.5.22-release_DEFAULT.apk'
+
+    context.driver = webdriver.Remote('http://0.0.0.0:4723/wd/hub', desired_caps)
+
