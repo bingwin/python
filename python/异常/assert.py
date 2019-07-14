939160af45d53f7b9629d620da5fcde50c256e1b
huangzetao <huangzetao@kuaishou.com>
2019-07-14 22:40:01 +0800

    assert脚本修改

--- python/异常/assert.py
+++ python/异常/assert.py
@@ -8,7 +8,14 @@ print(3)
 print(i)                # 按向下箭头进行调试
 print(4)
 print(5)
+a = 1
+b = 2
 
-assert 1 == 1
-assert 1 == 0
-# assert 1 == 0   #相当于if,false会报错
+
+assert 1 == 1   # 正常
+# assert 1 == 0   # 如果编写时是错误的 后面的一行会提示错误
+
+assert a == b   # 如果无法办法 后面的一行不会提示错误    运行时才提示错误
+
+# assert 1 == 0   #相当于if,false会报错   程序停止
+print(6)
